e3c88de1328179d3d2974cbd7e40401676023023
shahida-vs <shahida.khatoon@vectoscalar.com>
2020-05-09 21:40:38 +0530

    second commit

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.listen(3003,()=>console.log('Server Started'));
+app.listen(3005,()=>console.log('Server Started'));
 app.get('/abc', function (req, res) {
     console.log(req);
     res.send('Hello World');
