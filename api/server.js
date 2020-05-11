fe6a8c66042efb24c8224d7c77e2c641fe501c45
shahida-vs <shahida.khatoon@vectoscalar.com>
2020-05-11 17:48:47 +0530

    second commit

--- api/server.js
+++ api/server.js
@@ -1,8 +1,43 @@
+const moment = require('moment');
 const express = require('express');
 const app = express();
 
-app.listen(3005,()=>console.log('Server Started'));
-app.get('/abc', function (req, res) {
-    console.log(req);
-    res.send('Hello World');
+app.listen(3006,()=>console.log('Server Started'));
+
+getCurrentTimeFromStamp = (timestamp) => {
+   return new Date(parseInt(timestamp,10))
+};
+const isoToUnix = (d) => {
+    console.log("date",d);
+        return Date.parse(d)
+}
+app.get('/abc/:date?',  (req, res) => {
+    let unix_stamp = '';
+    let string_date = '';
+    if(req.params.date){
+        console.log("entered",(req.params.date) == 'NaN');
+        
+        //console.log("came",new Date(req.params.date));
+        
+      //if(new Date(req.params.date) != 'Invalid Date'){  
+          if(isNaN(req.params.date)){
+              console.log("here it is");
+              
+              unix_stamp = isoToUnix(req.params.date);
+            string_date = req.params.date;
+          }
+          else{
+              console.log("no here");
+              
+              string_date = getCurrentTimeFromStamp(req.params.date);
+              unix_stamp = req.params.date;
+          }
+      //}
+    }
+    else {
+        string_date = (new Date());
+        unix_stamp = Date.parse(string_date);
+    }
+    //const dt1 = isoToUnix(req.params.date);
+    res.send(`{ "unix" : ${unix_stamp} , "utc" : ${string_date}`);
  })
